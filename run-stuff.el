be08fb564ec563040aebe2872efa8ab7f040e8f6
Campbell Barton <ideasman42@gmail.com>
2017-07-30 07:38:44 +1000

    Initial template

---
+++ run-stuff.el
@@ -0,0 +1,31 @@
+;;; run-stuff.el --- a simple package                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Campbell Barton
+
+;; Author: Campbell Barton <ideasman42@gmail.com>
+;; Keywords: lisp
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Run commands from the region or current line,
+;; with some simple specifiers to control behavior.
+
+;;; Code:
+
+
+(provide 'run-stuff)
+;;; run-stuff.el ends here
